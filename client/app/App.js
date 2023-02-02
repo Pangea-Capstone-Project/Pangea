8c2474d0a0ec7d4e3d4dc4ab792d2a91d49a8a16
Kyle Colangelo <kyle123c@hotmail.com>
2023-02-02 16:46:26 -0500

    dashboard tweets

--- client/app/App.js
+++ client/app/App.js
@@ -1,17 +1,12 @@
-import React from 'react';
-
-import LandlordNavbar from '../features/navbar/LandlordNavbar';
-// import TenantNavbar from '../features/navbar/Tenantnavbar';
-import AppRoutes from './AppRoutes';
-import Footer from '../features/footer/Footer';
+import React from "react";
+import AppRoutes from "./AppRoutes";
+import Footer from "../features/footer/Footer";
 
 const App = () => {
   return (
     <div>
-      <LandlordNavbar />
       <AppRoutes />
       <Footer />
-      {/* dont mind me */}
     </div>
   );
 };
